46a40f968eac59d4c55c82ae87c7578c73c5f53e
nalvazhuthi <nalvazhuthi03@gmail.com>
2026-03-08 23:23:25 +0530

    updated roadmap and js

--- javascript/script.js
+++ javascript/script.js
@@ -1,54 +1,7 @@
-// Variables
-const name = "Nanni";
-age = 24;
+getName();
+console.log(x);
 
-console.log("My name is " + name + "i am " + age);
-
-console.log(`My name is 4 ${name} i am ${age}`);
-
-console.log("Length", name.length);
-console.log("Upper Case", name.toUpperCase());
-console.log("divide Case", name.substring(0, 3));
-console.log("Split", name.split(""));
-
-// Array
-const users = ["Nanni", "Nanni1", "Nanni 2"];
-
-users.push("nanni");
-
-users.unshift("nanni0");
-
-users.pop();
-console.log('users.indexOf("nanni"): ', users.indexOf("nanni0"));
-console.log(users);
-
-// Object
-const person = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 24,
-  skills: ["HTML", "CSS", "JS", "React JS"],
-  address: {
-    no: 3,
-    street: "AV Coloney, Thannerkulam",
-    district: "Thiruvallur",
-  },
+var x = 7;
+var getName = () => {
+  console.log("Hello World");
 };
-
-console.log(person.address.district);
-const {
-  firstName,
-  lastName,
-  age: userAge,
-  skills,
-  address: { no, street, district },
-} = person;
-
-console.log(street);
-console.log(JSON.stringify(person));
-
-// Loops
-
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
-}
